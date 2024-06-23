75be88b77381b7983bc525a03c1294bd06f3271e
Luiz Carlos <lsouzapedroso@gmail.com>
2024-06-23 17:40:18 -0300

    Created configs and pom files

---
+++ src/main/java/org/unicentro/CarsApiApplication.java
@@ -0,0 +1,9 @@
+package org.unicentro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class CarsApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarsApiApplication.class, args);    }
+}
