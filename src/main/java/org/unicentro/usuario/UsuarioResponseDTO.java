df77f2c0fee9dcd7d3a2b25d34a612b74a9d7b18
Ermindo Treviso Neto <ermindone@gmail.com>
2024-06-23 17:00:12 -0300

    criar DTO usuario ^^

---
+++ src/main/java/org/unicentro/usuario/UsuarioResponseDTO.java
@@ -0,0 +1,42 @@
+package org.unicentro.usuario;
+
+import java.util.Date;
+
+public class UsuarioResponseDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private Date dataCadastro;
+    private Date dataInativacao;
+    
+    public Long getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+    public Date getDataInativacao() {
+        return dataInativacao;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+    public void setDataInativacao(Date dataInativacao) {
+        this.dataInativacao = dataInativacao;
+    }
+}
